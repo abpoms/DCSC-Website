66465808244a94838768b3d155228e297acf2c98
Alex Poms <alexpoms@gmail.com>
2013-09-07 21:44:22 -0700

    Add requirements.txt for python virtualenv and error handling for email submission.

--- index.py
+++ index.py
@@ -3,9 +3,13 @@ import os
 os.chdir(os.path.dirname(__file__))
 
 # ... build or import your bottle application here ...
-from bottle import route, default_app, view, request, post
 import bottle
 import bottle_pgsql
+import formencode
+import psycopg2
+from formencode import validators
+from bottle import route, default_app, view, request, post
+
 
 plugin = bottle_pgsql.Plugin('dbname=web_user user=officer password=')
 default_app().install(plugin)
@@ -15,7 +19,22 @@ default_app().install(plugin)
 def do_index(db):
     email = request.forms.get('email')
     #TODO: error handling for db call
-    db.execute("INSERT INTO signup_emails VALUES (%s)", [email])
+    email_validator = validators.Email()
+    try:
+        email = email_validator.to_python(email)
+    except formencode.Invalid, e:
+        return bottle.template('index', submit=False, error_msg=e.msg)
+
+    try:
+        db.execute("INSERT INTO signup_emails VALUES (%s)", [email])
+    except psycopg2.IntegrityError, e:
+        return bottle.template('index', submit=False)
+    except psycopg2.DatabaseError, e:
+        return bottle.template(
+            'index',
+            submit=False,
+            error_msg='Can not add email right now, try again later.')
+
     return bottle.template('index', submit=True)
 
 
@@ -23,7 +42,6 @@ def do_index(db):
 @view('index')
 def index():
     return dict(submit=False)
-
 
 # Do NOT use bottle.run() with mod_wsgi
 application = default_app()
